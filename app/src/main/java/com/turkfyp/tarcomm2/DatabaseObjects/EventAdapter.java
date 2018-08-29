82a42ba51a0624f926d2427d1ab0ed91e84a4f80
kenyong1997 <kenyong1997@gmail.com>
2018-08-30 01:59:29 +0800

    d

---
+++ app/src/main/java/com/turkfyp/tarcomm2/DatabaseObjects/EventAdapter.java
@@ -0,0 +1,95 @@
+package com.turkfyp.tarcomm2.DatabaseObjects;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+import com.turkfyp.tarcomm2.R;
+
+
+/**
+ * Created by User-PC on 7/10/2017.
+ */
+
+public class EventAdapter extends ArrayAdapter<Event> {
+
+    public EventAdapter(Activity context, int resource, List<Event> list) {
+        super(context, resource, list);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Event event = getItem(position);
+
+        LayoutInflater inflater = (LayoutInflater) getContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View rowView = inflater.inflate(R.layout.event_records, parent, false);
+
+        TextView tvEventName, tvEventDateTime;
+        ImageView ivImageEvent;
+
+        tvEventName = (TextView) rowView.findViewById(R.id.tvEventName);
+        tvEventDateTime = (TextView) rowView.findViewById(R.id.tvEventDateTime);
+        ivImageEvent = (ImageView) rowView.findViewById(R.id.ivImageEvent);
+
+        tvEventName.setText(event.getEventName());
+        tvEventDateTime.setText(event.getEventDateTime());
+        getImage(event.getEventImageURL(), ivImageEvent);
+
+
+        return rowView;
+    }
+
+    private void getImage(String urlToImage, final ImageView ivImage) {
+        class GetImage extends AsyncTask<String, Void, Bitmap> {
+            ProgressDialog loading;
+
+            @Override
+            protected Bitmap doInBackground(String... params) {
+                URL url = null;
+                Bitmap image = null;
+
+                String urlToImage = params[0];
+                try {
+                    url = new URL(urlToImage);
+                    image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return image;
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                loading = ProgressDialog.show(getContext(), "Downloading Image...", "Please wait...", true, true);
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                super.onPostExecute(bitmap);
+                loading.dismiss();
+                ivImage.setImageBitmap(bitmap);
+            }
+        }
+        GetImage gi = new GetImage();
+        gi.execute(urlToImage);
+    }
+}
