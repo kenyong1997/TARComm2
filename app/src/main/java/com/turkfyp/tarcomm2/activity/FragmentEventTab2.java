c4dc69180deff3ebc83648f9b10ed317d9c2e01c
Wern Min <grayx3@hotmail.com>
2018-08-30 03:55:30 +0800

    done event and lostandfound frame

---
+++ app/src/main/java/com/turkfyp/tarcomm2/activity/FragmentEventTab2.java
@@ -0,0 +1,29 @@
+package com.turkfyp.tarcomm2.activity;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.turkfyp.tarcomm2.R;
+
+public class FragmentEventTab2 extends Fragment {
+
+    public FragmentEventTab2() {    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event_tab2, container, false);
+    }
+
+}
