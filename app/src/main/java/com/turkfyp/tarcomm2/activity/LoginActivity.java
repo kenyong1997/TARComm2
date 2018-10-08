7d3ba08fbc5307ab21a2d2630a694583334474ba
kenyong1997 <kenyong1997@gmail.com>
2018-10-08 23:20:02 +0800

    i

---
+++ app/src/main/java/com/turkfyp/tarcomm2/activity/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.turkfyp.tarcomm2.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import com.turkfyp.tarcomm2.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
