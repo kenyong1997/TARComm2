5d03b5c6bd3494439ee357a2980051e2d012279b
kenyong1997 <kenyong1997@gmail.com>
2018-10-29 01:37:30 +0800

    View other profile ui done, cannot view yet

--- app/src/main/java/com/turkfyp/tarcomm2/activity/LostFoundDetailActivity.java
+++ app/src/main/java/com/turkfyp/tarcomm2/activity/LostFoundDetailActivity.java
@@ -43,6 +43,7 @@ public class LostFoundDetailActivity extends AppCompatActivity {
     int lostID;
     String confirmation;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
